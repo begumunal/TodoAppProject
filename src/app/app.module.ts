9685e636dabafa6398fcd4e386463b4453656eb6
begüm ünal <bgm.unl00@gmail.com>
2021-07-07 13:29:58 +0300

    Lesson2: creating layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+//angular içerisinden gelen paketlerin birleşim alanı ve aynı 
+//zamanda hazırlamış olduğumuz companentleri barındırır  
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  declarations: [//farklı bir companent tanımlandığında declaration içine alınır . 
     AppComponent
   ],
-  imports: [
+  imports: [//farklı bir angular modulü kullanılırsa bu import kısmına alınır 
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent]//bu modül içerisindeki çalıştırılabilir ilk companent buraya yazılır. 
 })
-export class AppModule { }
+export class AppModule { }//export ile dışarıya açılır. 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { AppModule } from './app/app.module'; //appModule ün konumu import edilmiş
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
+//uygulamanın bir tarayıcıda çalışacağını ve 
+//başlangıçta da appModule ü çalıştıracağınıo söylediğimiz kısım 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
